ad48c5b4d2d1286fb8e6b0b3fe3ce6ebdd91e5f7
Samuel Cormier-Iijima <samuel@cormier-iijima.com>
2023-06-14 19:53:15 -0400

    Add typing for prosemirror.utils and add JSON alias

--- prosemirror/utils.py
+++ prosemirror/utils.py
@@ -1,2 +1,13 @@
-def text_length(text):
+from collections.abc import Mapping, Sequence
+from typing import Union
+
+from typing_extensions import TypeAlias
+
+JSONDict: TypeAlias = Mapping[str, "JSON"]
+JSONList: TypeAlias = Sequence["JSON"]
+
+JSON: TypeAlias = Union[JSONDict, JSONList, str, int, float, bool, None]
+
+
+def text_length(text: str) -> int:
     return len(text.encode("utf-16-le")) // 2
