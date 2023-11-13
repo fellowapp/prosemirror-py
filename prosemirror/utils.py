f1bda0000d6fb2ef8b947ceba435ff31b096dd6e
Ernesto Ferro <ernesto@fellow.co>
2023-11-13 14:33:43 -0500

    Removing mutable json types. Creating new immutable instances instead of mutating existing ones.

--- prosemirror/utils.py
+++ prosemirror/utils.py
@@ -1,18 +1,11 @@
-from typing import Mapping, MutableMapping, MutableSequence, Sequence, Union
+from typing import Mapping, Sequence
 
 from typing_extensions import TypeAlias
 
 JSONDict: TypeAlias = Mapping[str, "JSON"]
 JSONList: TypeAlias = Sequence["JSON"]
 
-JSON: TypeAlias = Union[JSONDict, JSONList, str, int, float, bool, None]
-
-MutableJSONDict: TypeAlias = MutableMapping[str, "MutableJSON"]
-MutableJSONList: TypeAlias = MutableSequence["MutableJSON"]
-
-MutableJSON: TypeAlias = Union[
-    MutableJSONDict, MutableJSONList, str, int, float, bool, None
-]
+JSON: TypeAlias = JSONDict | JSONList | str | int | float | bool | None
 
 
 def text_length(text: str) -> int:
